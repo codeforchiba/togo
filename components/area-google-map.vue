7e8c8ee37723c54baa221c563aea08e7509f5541
seitaro <seitaro.hrkw@gmail.com>
2020-04-30 19:38:53 +0900

    area-google-map.vueで@Componentを追加

--- components/area-google-map.vue
+++ components/area-google-map.vue
@@ -1,19 +1,19 @@
 <template>
-  <div style="height: 25rem;background-color: #7F828B;color: white">
+  <div style="height: 25rem;background-color: #7F828B;color: white" :data-aaa="geoJson">
     Google Map
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
-import { Prop } from 'nuxt-property-decorator'
+import { Component, Prop } from 'nuxt-property-decorator'
 import Shop from '~/models/shop'
 import ShopGeoJson from '~/models/shop-geo-json'
 
+@Component
 export default class AreaGoogleMap extends Vue {
-  @Prop({ required: true })
-  shops!: ReadonlyArray<Shop>
+  @Prop({ required: true }) readonly shops!: ReadonlyArray<Shop>
 
-  get geoJson () {
+  get geoJson (): ShopGeoJson {
     return new ShopGeoJson(this.shops)
   }
 }
