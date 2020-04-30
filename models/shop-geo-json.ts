68d7ea4a5fa39edd696b0425884c7c96a15cdfe6
seitaro <seitaro.hrkw@gmail.com>
2020-04-30 19:14:47 +0900

    ShopGeoJsonをDefinitelyTypedを使って実装

--- models/shop-geo-json.ts
+++ models/shop-geo-json.ts
@@ -1,56 +1,28 @@
+import { Feature, FeatureCollection, Point } from 'geojson'
 import Shop from '~/models/shop'
 
-interface GeoJsonGeometry {
-  type: string
-  coordinates: any
-}
+class ShopFeature implements Feature<Point, Shop> {
+  readonly type = 'Feature'
+  readonly geometry: Point
+  readonly properties: Shop
 
-class GeoJsonPointGeometry implements GeoJsonGeometry {
-  readonly type: string = 'Point';
-  private readonly lat: number;
-  private readonly lng: number;
-
-  constructor (lat: number, lng: number) {
-    this.lat = lat
-    this.lng = lng
-  }
-
-  coordinates (): Array<number> {
-    return [this.lat, this.lng]
-  }
-}
-
-interface GeoJsonFeature {
-  type: string
-  geometry: GeoJsonGeometry
-  properties: any
-}
-
-class GeoJsonPointFeature implements GeoJsonFeature {
-  readonly type: string = 'Feature';
-  readonly geometry: GeoJsonPointGeometry;
-  readonly properties: any;
-
-  constructor (geometry: GeoJsonPointGeometry, properties: any) {
-    this.geometry = geometry
+  constructor (lat: number, lng: number, properties: Shop) {
+    this.geometry = { type: 'Point', coordinates: [lat, lng] }
     this.properties = properties
   }
 }
 
-class ShopGeoJson {
-  readonly type: string = 'FeatureCollection';
-  features: ReadonlyArray<GeoJsonFeature> = [];
+class ShopGeoJson implements FeatureCollection<Point, Shop> {
+  readonly type = 'FeatureCollection'
+  readonly features: Array<Feature<Point, Shop>>
 
   constructor (shops: ReadonlyArray<Shop>) {
     this.features = shops
       .filter(shop => (typeof shop.lat === 'number') && (typeof shop.lng === 'number'))
       .map((shop) => {
-        // TODO: shop.lat shop.lng の型変換 or null ガード
-        // shopのほうがoptional型
-        // filterでガードしても型チェックで引っかかる。
-        // filterでガードして 非optional型にcastする?
-        const geoJsonPointGeometry = new GeoJsonPointGeometry(1, 1)
-        return new GeoJsonPointFeature(geoJsonPointGeometry, shop)
+        const lat = shop.lat as number
+        const lng = shop.lng as number
+        return new ShopFeature(lat, lng, shop)
       })
   }
 }
