34cc8c98e3e6bb656ca1b8444077b360ca46713d
kazuyukiHATANKAKA <cavy@nifty.com>
2020-12-31 21:27:09 +0900

    指摘事項の反映

--- components/logo.vue
+++ components/logo.vue
@@ -4,11 +4,7 @@
       <v-img :src="logoPath" alt="お家で食べよう" />
     </v-col>
     <v-col cols="12" class="text-center headline">
-      <p>サービス終了のお知らせ</p>
-    </v-col>
-    <v-col cols="12" class="text-left text-md-center">
-      <p>2020年4月より運営してまいりました「おうちで食べようin千葉」ですが、サービスの利用状況を鑑みて、2021年1月31日（日）をもちまして、サービスを終了させていただくこととなりました。</p>
-      <p>いままで「おうちで食べようin千葉」をご利用いただき、誠にありがとうございます。引き続き、code for chibaのサービスを利用いただけますことを心よりお待ち申し上げます。</p>
+      <p>近所のお店で買って、お家で食べよう！</p>
     </v-col>
   </v-row>
 </template>
